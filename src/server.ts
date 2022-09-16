d4d333b2e2262fa33051d108a58e8cc0646721ec
vcsil <vcsilva@poli.ufrj.br>
2022-09-15 23:41:21 -0300

    feat: create server

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+import app from './app';
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
